0739ab3265482ebd713d182dbe8fb2c859008f80
karan-tech07 <you@example.com>
2020-09-14 15:48:34 +0530

    Between

--- sketch.js
+++ sketch.js
@@ -6,6 +6,7 @@ function setup(){
     left = createSprite(5, displayHeight / 2, 10, displayHeight);
     top = createSprite(displayWidth/ 2, 5, displayWidth, 10);
     right = createSprite(displayWidth - 5, displayHeight / 2, 10, displayHeight);
+    between = createSprite(50, 100, 20, 400);
 
 }
 
